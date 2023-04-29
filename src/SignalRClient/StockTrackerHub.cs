1e727e3b5e54fb9cc314bbcd9bd671054da94d01
Thwani sithole <thulaniinnocent47@gmail.com>
2023-04-30 01:32:15 +0200

    Delete src/SignalRClient directory

--- src/SignalRClient/StockTrackerHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-public class StockTrackerHub : Hub
-{
-    public void SendMessage(string message)
-    {
-        Clients.All.SendAsync("message", message);
-    }
-}
